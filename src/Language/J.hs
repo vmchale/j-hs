88fa5b63dd9184919bf5a6b9f76b2907650b6523
Vanessa McHale <vamchale@gmail.com>
2020-10-05 16:31:28 -0500

    actual test case

--- src/Language/J.hs
+++ src/Language/J.hs
@@ -3,9 +3,12 @@ module Language.J ( J
                   , mkJDo
                   , mkJInit
                   , jinit
+                  , JType (..)
+                  , intToJType
                   ) where
 
 import           Foreign.C.String           (CString)
+import           Foreign.C.Types            (CInt (..), CLLong (..))
 import           Foreign.Ptr                (FunPtr, Ptr)
 -- TODO: windows support
 import           System.Posix.DynamicLinker (RTLDFlags (RTLD_LAZY), dlopen,
@@ -14,17 +17,42 @@ import           System.Posix.DynamicLinker (RTLDFlags (RTLD_LAZY), dlopen,
 data J
 
 data JEnv = JEnv { context   :: Ptr J
-                 , evaluator :: Ptr J -> CString -> IO ()
-                 -- TODO: jgetb or whatever
+                 , evaluator :: JDoType
+                 , reader    :: JGetMType
+                 -- TODO: jgetb jor whatever
                  }
 
-foreign import ccall "dynamic" mkJDo :: FunPtr (Ptr J -> CString -> IO ()) -> Ptr J -> CString -> IO ()
+type JDoType = Ptr J -> CString -> IO CInt
+type JGetMType = Ptr J -> CString -> Ptr CLLong -> Ptr CLLong -> Ptr CLLong -> Ptr CLLong -> IO CInt
+
+-- FIXME: check for error?
+
+foreign import ccall "dynamic" mkJDo :: FunPtr JDoType -> JDoType
 foreign import ccall "dynamic" mkJInit :: FunPtr (IO (Ptr J)) -> IO (Ptr J)
+foreign import ccall "dynamic" mkJGetM :: FunPtr JGetMType -> JGetMType
 
 -- only works on linux
 jinit :: IO JEnv
 jinit = do
     libj <- dlopen "/usr/lib/x86_64-linux-gnu/libj.so" [RTLD_LAZY]
     jt <- mkJInit =<< dlsym libj "JInit"
-    let jeval = mkJDo <$>  dlsym libj "JDo"
-    JEnv jt <$> jeval
+    let jeval = mkJDo <$> dlsym libj "JDo"
+    let jread = mkJGetM <$> dlsym libj "JGetM"
+    JEnv jt <$> jeval <*> jread
+
+
+data JType = JBool
+           | JChar
+           | JInteger
+           | JDouble
+           | JComplex
+           | JBoxed
+           deriving (Show, Eq)
+
+intToJType :: CLLong -> JType
+intToJType 1  = JBool
+intToJType 2  = JChar
+intToJType 4  = JInteger
+intToJType 8  = JDouble
+intToJType 16 = JComplex
+intToJType 32 = JBoxed
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,29 @@
 module Main ( main ) where
 
-import           Data.Functor     (void)
-import           Foreign.C.String (withCString)
+import           Data.Functor          (void)
+import           Foreign.C.String      (withCString)
+import           Foreign.Marshal.Alloc (alloca)
+import           Foreign.Storable      (peek)
 import           Language.J
+import           Test.Tasty
+import           Test.Tasty.HUnit
 
 main :: IO ()
-main = do
-    (JEnv ctx jdo) <- jinit
-    withCString "print =: monad : 'y (1!:2) 2';a =: 6?6;print a" $
+main =
+    defaultMain $
+        testGroup "J dl"
+            [ testCase "Reads back type in the environment" jType ]
+
+jType :: Assertion
+jType = do
+    (JEnv ctx jdo jget) <- jinit
+    withCString "a =: 6?6" $
         jdo ctx
+    withCString "a" $ \a ->
+        alloca $ \t ->
+        alloca $ \s ->
+        alloca $ \r ->
+        alloca $ \d -> do
+            jget ctx a t s r d
+            t' <- peek t
+            intToJType t' @?= JInteger
