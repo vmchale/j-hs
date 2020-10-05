f636392291a8eb420e2aef7ada2b6d32d879f90e
Vanessa McHale <vamchale@gmail.com>
2020-10-05 17:58:00 -0500

    print-y things

--- src/Language/J.hs
+++ src/Language/J.hs
@@ -1,46 +1,70 @@
-module Language.J ( J
-                  , JEnv (..)
+module Language.J ( -- * Environment
+                    JEnv (..)
+                  , jinit
+                  , jinitLinux
+                  , bsDispatch
+                  , bsOut
+                  -- * FFI
+                  , JDoType
+                  , JGetMType
+                  , J
                   , mkJDo
                   , mkJInit
-                  , jinit
+                  , mkJGetM
+                  -- * Marshaling (to Haskell)
                   , JType (..)
                   , intToJType
                   ) where
 
+import           Control.Applicative        ((<$>), (<*>))
+import           Data.ByteString            as BS
+import           Data.Functor               (void)
 import           Foreign.C.String           (CString)
 import           Foreign.C.Types            (CInt (..), CLLong (..))
 import           Foreign.Ptr                (FunPtr, Ptr)
--- TODO: windows support
 import           System.Posix.DynamicLinker (RTLDFlags (RTLD_LAZY), dlopen,
                                              dlsym)
+-- TODO: windows support
 
 data J
 
 data JEnv = JEnv { context   :: Ptr J
                  , evaluator :: JDoType
                  , reader    :: JGetMType
-                 -- TODO: jgetb jor whatever
+                 , output    :: JGetRType
                  }
 
 type JDoType = Ptr J -> CString -> IO CInt
 type JGetMType = Ptr J -> CString -> Ptr CLLong -> Ptr CLLong -> Ptr CLLong -> Ptr CLLong -> IO CInt
-
--- FIXME: check for error?
+type JGetRType = Ptr J -> IO CString
 
 foreign import ccall "dynamic" mkJDo :: FunPtr JDoType -> JDoType
 foreign import ccall "dynamic" mkJInit :: FunPtr (IO (Ptr J)) -> IO (Ptr J)
 foreign import ccall "dynamic" mkJGetM :: FunPtr JGetMType -> JGetMType
+foreign import ccall "dynamic" mkJGetR :: FunPtr JGetRType -> JGetRType
 
--- only works on linux
-jinit :: IO JEnv
-jinit = do
-    libj <- dlopen "/usr/lib/x86_64-linux-gnu/libj.so" [RTLD_LAZY]
+jinitLinux :: IO JEnv
+jinitLinux = jinit "/usr/lib/x86_64-linux-gnu/libj.so"
+
+-- | Get a J environment
+jinit :: FilePath -- ^ Path to J library
+      -> IO JEnv
+jinit libFp = do
+    libj <- dlopen libFp [RTLD_LAZY]
     jt <- mkJInit =<< dlsym libj "JInit"
     let jeval = mkJDo <$> dlsym libj "JDo"
     let jread = mkJGetM <$> dlsym libj "JGetM"
-    JEnv jt <$> jeval <*> jread
+    let jOut = mkJGetR <$> dlsym libj "JGetR"
+    JEnv jt <$> jeval <*> jread <*> jOut
 
+bsDispatch :: JEnv -> BS.ByteString -> IO ()
+bsDispatch (JEnv ctx jdo _ _) bs =
+    void $ BS.useAsCString bs $ jdo ctx
 
+bsOut :: JEnv -> IO BS.ByteString
+bsOut (JEnv ctx _ _ jout) = BS.packCString =<< jout ctx
+
+-- | J types
 data JType = JBool
            | JChar
            | JInteger
@@ -56,3 +80,4 @@ intToJType 4  = JInteger
 intToJType 8  = JDouble
 intToJType 16 = JComplex
 intToJType 32 = JBoxed
+intToJType _  = error "Bad or unknown type!"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main ( main ) where
 
-import           Data.Functor          (void)
-import           Foreign.C.String      (withCString)
+import qualified Data.ByteString       as BS
 import           Foreign.Marshal.Alloc (alloca)
 import           Foreign.Storable      (peek)
 import           Language.J
@@ -9,17 +10,28 @@ import           Test.Tasty
 import           Test.Tasty.HUnit
 
 main :: IO ()
-main =
+main = do
+    jenv <- jinitLinux
     defaultMain $
         testGroup "J dl"
-            [ testCase "Reads back type in the environment" jType ]
+            [ testCase "Performs calculation and has sensible output" (jComp jenv)
+            , testCase "Reads back type in the environment" (jType jenv)
+            ]
 
-jType :: Assertion
-jType = do
-    (JEnv ctx jdo jget) <- jinit
-    withCString "a =: 6?6" $
+jComp :: JEnv -> Assertion
+jComp (JEnv ctx jdo _ jout) = do
+    BS.useAsCString "harmonic =: (+/ % #) &.: %" $
         jdo ctx
-    withCString "a" $ \a ->
+    BS.useAsCString "harmonic 1 3 6" $
+        jdo ctx
+    res <- BS.packCString =<< jout ctx
+    res @?= "2\n"
+
+jType :: JEnv -> Assertion
+jType (JEnv ctx jdo jget _) = do
+    BS.useAsCString "a =: 6?6" $
+        jdo ctx
+    BS.useAsCString "a" $ \a ->
         alloca $ \t ->
         alloca $ \s ->
         alloca $ \r ->
